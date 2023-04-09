9d36864a1a980010892d742d0cc1dee88d344c40
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2023-04-09 10:11:53 -0300

    Initial

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APICatalogo.Models;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+    }
+}
